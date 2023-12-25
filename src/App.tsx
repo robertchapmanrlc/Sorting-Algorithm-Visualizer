c490063a85614e8c0cbd27051797c8592efba86a
Robert Chapman <chapmanrobert12@gmail.com>
2023-12-25 14:31:29 -0600

    Uses state variable for the number of bars to sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { useEffect, useState } from "react";
+
 import Dashboard from "./components/Dashboard/Dashboard";
 import Explanation from "./components/Explanation/Explanation";
 import SortingBars from "./components/SortingBars/SortingBars";
 
 function App() {
-  const bars: number[] = Array(16);
+  const [bars, setBars] = useState(Array(16).fill(0));
 
-  for (let i = 0; i < bars.length; i += 1) {
-    bars[i] = i + 1;
-  }
+  useEffect(() => {
+    let barsCopy = [...bars];
+    for (let i = 0; i < bars.length; i += 1) {
+      barsCopy[i] = i + 1;
+    }
+    setBars([...barsCopy]);
+  });
 
   return (
     <>
